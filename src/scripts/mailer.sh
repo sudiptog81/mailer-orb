05d74945594ec7c29ab0f528e095d5d213c41d14
Sudipto Ghosh <sudipto@ghosh.pro>
2020-10-08 10:53:01 +0530

    changed shell script

--- src/scripts/mailer.sh
+++ src/scripts/mailer.sh
@@ -5,6 +5,12 @@ root=$PARAM_FROM
 mailhub=$SMTP_HOST:587
 AuthUser=$SMTP_USER
 AuthPass=$SMTP_PASS
+FromLineOverride=yes
+EOF
+
+cat << EOF  | sudo tee /etc/ssmtp/revaliases
+root:$PARAM_FROM:$SMTP_HOST:587
+circleci:$PARAM_FROM:$SMTP_HOST:587
 EOF
 
 cat << EOF > email.txt
@@ -12,4 +18,4 @@ Subject: $PARAM_SUBJECT
 Hi. This is a test!
 EOF
 
-sendmail $PARAM_FROM < email.txt
+sendmail $PARAM_TO < email.txt
