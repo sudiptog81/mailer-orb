6bf27e06ed54e2b7c2052cc2e8dc3ca066a655ec
Sudipto Ghosh <sudipto@ghosh.pro>
2020-10-08 08:32:58 +0530

    initial test

---
+++ src/scripts/mailer.js
@@ -0,0 +1,9 @@
+const {
+    PARAM_TO: mailTo,
+    PARAM_FROM: mailFrom,
+    PARAM_SMTP_HOST: smtpHost,
+    PARAM_SMTP_USER: smtpUser,
+    PARAM_SMTP_PASS: smtpPass
+} = process.env;
+
+console.log(mailTo, mailFrom, smtpHost);
