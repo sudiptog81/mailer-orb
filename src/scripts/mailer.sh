790ab9c23814d372f40443758c77dbff3f72a9be
Sudipto Ghosh <sudipto@ghosh.pro>
2020-10-08 09:52:20 +0530

    fixed shell script

---
+++ src/scripts/mailer.sh
@@ -0,0 +1,17 @@
+sudo apt install ssmtp
+
+sudo cat << EOF > /etc/ssmtp/ssmtp.conf
+UseSTARTTLS=YES
+FromLineOverride=YES
+root=$PARAM_FROM
+mailhub=$PARAM_SMTP_HOST:587
+AuthUser=$PARAM_SMTP_USER
+AuthPass=$PARAM_SMTP_PASS
+EOF
+
+cat << EOF > email.txt
+Subject: Mail from mailer-orb
+Hi. This is a test!
+EOF
+
+sendmail $PARAM_FROM < email.txt
